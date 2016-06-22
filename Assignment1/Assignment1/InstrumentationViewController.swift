f6f77e0116fa8b56fb20859f3185af155f2d0317
celia-tang <Celia@celias-macbook-pro-2.local>
2016-06-22 16:40:06 -0400

    assignment 1

---
+++ Assignment1/Assignment1/InstrumentationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Assignment1
+//
+//  Created by Celia Tang on 2016-06-20.
+//  Copyright © 2016 Celia Tang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
